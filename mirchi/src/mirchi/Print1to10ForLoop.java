893ab598757aa05c209f02768d4f7eb606d78da2
rpallerla <rpallerla@matson.com>
2019-10-01 21:54:32 -0400

    second commit

---
+++ mirchi/src/mirchi/Print1to10ForLoop.java
@@ -0,0 +1,11 @@
+package mirchi;
+
+public class Print1to10ForLoop {
+	public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
